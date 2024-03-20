ab73da1890b6fd00af6aec62774e393a8e690efe
Deep Majumder <deep.majumder2019@gmail.com>
2024-03-20 18:07:05 +0530

    Fixed build.rs

--- build.rs
+++ build.rs
@@ -1,9 +1,16 @@
 fn main() {
+    #[cfg(target_os = "macos")]
+    {
+	const FRAMEWORKS: [&str; 3] = ["IOKit", "Cocoa", "OpenGL"];
+	for framework in FRAMEWORKS {
+	    println!("cargo:rustc-link-lib=framework={}", framework);
+	}
+    }
     let raylib = pkg_config::probe_library("raylib").unwrap();
     for link_path in &raylib.link_paths {
         println!("cargo:rustc-link-search={}", link_path.display());
     }
     for lib in &raylib.libs {
-        println!("cargo:rust-link-lib={}", lib);
+        println!("cargo:rustc-link-lib={}", lib);
     }
 }
